7bafa940a498b32359e63d635d56e7dfe1776dab
japolyak <artemkryvolap@gmail.com>
2023-03-24 17:39:55 +0100

    creating ORM

--- backplat/models.py
+++ backplat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Subject(models.Model):
+    id = models.IntegerField(db_column='id', primary_key=True)
+    subject_name = models.CharField(max_length=200, db_column='subject_name')
+
+
+class TeacherSubject(models.Model):
+    teacher_id = models.ForeignKey(to_field='id')
+    subject_id = models.ForeignKey(Subject, on_delete=, to_field='id')

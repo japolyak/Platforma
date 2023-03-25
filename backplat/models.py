ae75d0c3e4903aa5f669947a22882d2d0d62efcb
japolyak <artemkryvolap@gmail.com>
2023-03-25 17:55:45 +0100

    Modelso creating

--- backplat/models.py
+++ backplat/models.py
@@ -1,11 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
-    id = models.IntegerField(db_column='id', primary_key=True)
-    subject_name = models.CharField(max_length=200, db_column='subject_name')
+    subject_name = models.CharField(max_length=200)
 
 
 class TeacherSubject(models.Model):
-    teacher_id = models.ForeignKey(to_field='id')
-    subject_id = models.ForeignKey(Subject, on_delete=, to_field='id')
+    teacher = models.ForeignKey(User, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+
+
+class Group(models.Model):
+    group_name = models.CharField(max_length=255)
+    teacher = models.ForeignKey(User, on_delete=models.PROTECT)
+    subject = models.ForeignKey(Subject, on_delete=models.PROTECT)
+
+
+class StudentGroup(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
+    student = models.ForeignKey(User, on_delete=models.PROTECT)
+
+
+class Assignment(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.PROTECT)
+    assign_deadline = models.DateTimeField()
+    assign_text = models.CharField(max_length=1024, null=True)
+
+
+class Mark(models.Model):
+    assignment = models.ForeignKey(Assignment, on_delete=models.PROTECT)
+    student = models.ForeignKey(User, on_delete=models.PROTECT)
+    mark = models.IntegerField(blank=True)
